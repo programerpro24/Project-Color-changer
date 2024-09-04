bc573c7ad1ed4935554548397431fe3705b7e7a5
programerpro24 <shivamsrivastava.engg2022@gmail.com>
2024-09-04 23:33:29 +0530

    Add files

---
+++ script.js
@@ -0,0 +1,19 @@
+const buttons = document.querySelectorAll(".button");
+const body = document.querySelector("body");
+
+buttons.forEach(function(button) {
+  button.addEventListener('click', function(e) {
+    if (e.target.id === 'grey') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'white') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'blue') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'yellow') {
+      body.style.backgroundColor = e.target.id;
+    }
+  });
+});
